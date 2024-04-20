3f006facca068b8664c5e2c65f50e3d42bf048a9
Chabani Mohamed Samy <167400040+Mr-Mohamed-Samy@users.noreply.github.com>
2024-04-20 21:23:35 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+const guess = document.getElementById('guess'); 
+const att = document.getElementById('att'); 
+const corrans = document.getElementById('corrans'); 
+const btn = document.getElementById('btn');
+let attemps = 0;
+let min = 1;
+let max = 100;
+let rightNum = Math.floor(Math.random () * (max - min + 1)) + min;
+    btn.onclick = function(event) {
+    event.preventDefault();
+    attemps++;
+    att.textContent = attemps;
+    let theGuess = parseInt(guess.value);
+    if(theGuess > rightNum){
+        corrans.textContent = "YOUR GUESS IS GREATER THAN THE CORRECT ANSWER";
+    } else if(theGuess < rightNum){
+        corrans.textContent = "YOUR GUESS IS LESS THAN THE CORRECT ANSWER";
+    } else if(theGuess == rightNum){
+        corrans.textContent = "YOUR GUESS IS CORRECT";
+    }
+    if (attemps === 3) {
+        btn.disabled = true; 
+        guess.disabled = true; 
+    }
+};
+
+
